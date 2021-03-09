5b5fdb19e42e80b53e3de73ddd5c3819177acb1a
namelessup <fumin_chen@163.com>
2021-03-09 20:31:55 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,8 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
-	      "sub.svipstar.top (无名猪提供）": "https://sub.svipstar.top/sub?",
+          "sub.svipstar.top (无名猪提供）": "https://sub.svipstar.top/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -265,8 +264,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://sub-beta.now.sh/sub?" },
-	      { value: "https://sub.svipstar.top/sub?" },
+          { value: "https://sub.svipstar.top/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
